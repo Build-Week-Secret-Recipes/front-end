7a4ecc8999676a4e28f8d7da9fd3c85295833c27
Justin Bain <jbain912@gmail.com>
2021-07-24 16:56:12 -0500

    Done with Landing Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          
+        </Switch>
+      </Router>
+    </>
   );
 }
 
